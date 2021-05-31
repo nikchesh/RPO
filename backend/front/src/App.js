5287854f9c89c2ff89babc9582e128588bc30bae
nikchesh <ikolya-2000@mail.ru>
2021-05-31 19:57:55 +0300

    LR10

--- backend/front/src/App.js
+++ backend/front/src/App.js
@@ -1,25 +1,53 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
-import { Router, Switch, Route } from "react-router-dom";
+import { Router, Switch, Route, Redirect } from "react-router-dom";
 import { createBrowserHistory } from 'history';
 import NavigationBar from "./components/NavigationBar";
 import Home from "./components/Home";
 import Login from "./components/Login";
+import Utils from "./utils/Utils"
+import {connect} from "react-redux";
+import CountryListComponent from "./components/CountryListComponent";
+import CountryComponent from "./components/CountryComponent";
+import SideBar from "./components/SideBar";
 
-function App() {
+
+const AuthRoute = props => {
+    let user = Utils.getUserName();
+    if (!user) return <Redirect to="/login" />;
+    return <Route {...props} />;
+};
+
+const history = createBrowserHistory();
+
+function App(props) {
+    const [exp, setExpanded] = useState(false);
+
     return (
         <div className = "App">
-            <Router history = { createBrowserHistory() }>
-                <NavigationBar/>
-                <div className="container-fluid">
-                    <Switch>
-                        <Route path="/home" exact component={Home} />
-                        <Route path="/login" exact component={Login} />
-                    </Switch>
+            <Router history = { history }>
+                <NavigationBar toggleSideBar={()=>setExpanded(!exp)}/>
+                <div className="wrapper">
+                    <SideBar expanded={exp}/>
+                    <div className="container-fluid">
+                        {props.error_message &&
+                        <div className="alert alert-danger m-1">{props.error_message}</div>}
+                        <Switch>
+                            <AuthRoute path="/home" component={Home} />
+                            <AuthRoute path="/countries" exact component={CountryListComponent}/>
+                            <AuthRoute path="/countries/:id" component={CountryComponent}/>
+                            <Route path="/login" component={Login} />
+                        </Switch>
+                    </div>
                 </div>
             </Router>
         </div>
     );
 }
 
-export default App;
+function mapStateToProps(state) {
+    const {msg} = state.alert;
+    return {error_message: msg};
+}
+
+export default connect(mapStateToProps)(App);
